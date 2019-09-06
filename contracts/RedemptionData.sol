848be265d9ad36207e538882d7054a5787dce713
alexeykosinski <alexey.kosinski@nordwhale.com>
2019-09-06 11:40:03 +0300

    add functions awardUser and getUserBonuses to RedemptionData.sol

--- contracts/RedemptionData.sol
+++ contracts/RedemptionData.sol
@@ -2,20 +2,39 @@ pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./GoodDollar.sol";
 
 contract RedemptionData is Ownable {
+    using SafeMath for uint256;
     mapping(address => uint256) private last_claimed;
-
+    mapping(address => uint256) public rewarded;
+    GoodDollar public token;
 
     event SetClaim(address indexed Account, uint256 Time);
+    event BonusClaimed(address indexed Account, uint256 Amount);
 
     constructor() public {
+    }
+
+    function getUserBonuses(
+         address _user
+    ) public view returns(uint256) {
+        return rewarded[_user];
+    }
+
+    function awardUser(
+        address _user, uint256 _amount
+    ) public onlyOwner {
+        rewarded[_user] = rewarded[_user].add(_amount);
+        token.mint(_user, _amount);
+        emit BonusClaimed(_user, _amount);
     }
 
     function getLastClaimed(
         address _account
     ) public view onlyOwner returns(uint256) {
-        return last_claimed[_account]; 
+        return last_claimed[_account];
     }
 
     function setLastClaimed(
@@ -24,5 +43,5 @@ contract RedemptionData is Ownable {
         last_claimed[_account] = now;
         return true;
     }
-    
+
 }
